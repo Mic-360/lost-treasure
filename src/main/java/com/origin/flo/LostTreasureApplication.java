f37e24af0ba4843c2f97b9945a8898556cbd11d8
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-12-26 15:36:26 +0530

    Add initial Spring Boot application and configuration properties
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

---
+++ src/main/java/com/origin/flo/LostTreasureApplication.java
@@ -0,0 +1,13 @@
+package com.origin.flo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostTreasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostTreasureApplication.class, args);
+	}
+
+}
